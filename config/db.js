e2c51ff4dcfa9d1fa19dbca0f9ca9b973faad762
Factor <tu.email@example.com>
2025-06-02 16:33:53 -0500

    Corrijo nombre del archivo5

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
-// config/db.js
-import pkg from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const { Pool } = pkg;
+import pg from 'pg';
+const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false // necesario para Render
+  }
 });
 
 export default pool;
